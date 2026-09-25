d3b149038c8443337c655ee1ccdb829fdf3c0928
MiguelParedesR <miguelparedesramirez14@gmail.com>
2026-09-25 15:08:07 -0500

    style(redesign): load final Apple module overrides

--- react/src/app/App.jsx
+++ react/src/app/App.jsx
@@ -1,5 +1,6 @@
 import { RouterProvider } from 'react-router-dom';
 import router from './router.jsx';
+import '../styles/apple.css';
 
 export default function App() {
   return <RouterProvider router={router} />;
